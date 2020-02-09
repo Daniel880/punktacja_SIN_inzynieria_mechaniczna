db36e5974933c1827a1ed4ab110bb3984b7c23e3
Daniel Wyrwał <daniel.wyrwal@gmail.com>
2020-02-09 01:01:16 +0100

    Usprawnienie programu i poprawienie multithreadingu

--- pozyskiwanie_danych.py
+++ pozyskiwanie_danych.py
@@ -2,48 +2,88 @@ import urllib
 import re
 from tqdm import tqdm
 from threading import Thread 
+import os
 
+od_roku = '2018'
+liczba_watkow = 256
+liczba_stron_do_przejrzenia = 20
 
-lista_stron = ['https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=1',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=2',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=3',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=4',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=5',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=6',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=7',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=8',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=9',
-                'https://sin.put.poznan.pl/search/publications/all?yearFrom=2018&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page=10',
-               ]
+lista_stron =['https://sin.put.poznan.pl/search/publications/all?yearFrom={}&yearTo=2020&disciplineCode=inzynieria-mechaniczna&page={}'.format(od_roku, i) for i in range(liczba_stron_do_przejrzenia)]
 
 lista_id = []
 
+try:
+    os.makedirs("./publikacje/{}".format(od_roku))
+except:
+    pass
+
 cnt=0
+pbar = tqdm(total = len(lista_stron))
+
 for url in lista_stron:
     cnt+=1
-    print("pobieram dane strona:{}".format(cnt))
     req = urllib.request.urlopen(url).read().decode("UTF-8")
     pub_id = re.findall('href="/publications/details/(i\d{1,6})">', req)
     for i in pub_id:
         lista_id.append(i)
+    pbar.update(1)
     
-print("liczba publikacji:{}".format(len(lista_id)))
-
+print("\r\nliczba publikacji:{}\r\n".format(len(lista_id)))
 
 lista_autorow = {}
 
 
-def pobierz_html(art_id):
-    req = urllib.request.urlopen('https://sin.put.poznan.pl/publications/details/' + art_id).read()
 
-    with open('pub_18_20/' + art_id+'.html','wb') as f:
+            
+            
+
+def pobierz_html(art_id):
+    req = urllib.request.urlopen('https://sin.put.poznan.pl/publications/details/' + art_id, timeout = 600).read()
+
+    with open("./publikacje/{}/{}.html".format(od_roku, art_id),'wb') as f:
         f.write(req)
 
-watki = []
 
-for id_num in tqdm(range(len(lista_id))):
-    watki.append(Thread(target = pobierz_html, args =(lista_id[id_num])))
-    watki[-1].start()
+
+watki = []
     
-for i in tqdm(range(len(watki))):
-    watki[i].join()
+liczba_aktywnych_watkow = 0
+ostatni_uruchomiony_watek = 0
+liczba_art = len(lista_id)
+id_num = 0
+
+pbar = tqdm(total=100)
+
+while id_num < liczba_art or liczba_aktywnych_watkow > 0:
+    if liczba_aktywnych_watkow < liczba_watkow and id_num < liczba_art:
+        liczba_aktywnych_watkow += 1
+        watki.append(Thread(target = pobierz_html, args =(lista_id[id_num],)))
+        id_num += 1
+        watki[-1].start()
+        ostatni_uruchomiony_watek+=1
+    else:
+        for x in range(len(watki)):
+            if not watki[x].isAlive():
+                del watki[x]
+                liczba_aktywnych_watkow -= 1
+                pbar.update(1)
+                break
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
